da6d842892d093b844586558c916df5afa8ac93a
jahar123 <ajbiaseno@gmail.com>
2026-07-05 17:19:26 +0300

    убрал иф

--- main.js
+++ main.js
@@ -1,43 +1,41 @@
-const productCards = document.querySelectorAll('.product-card__box');
-const changeColorCardButton = document.querySelector('#change-color-all-card');
-const greenColorHash = '#00ff00';
-const yellowColorHash = '#FFFF00';
 
-if (changeColorCardButton) {
-    changeColorCardButton.addEventListener('click', () => {
-        productCards.forEach((card) => {
-            card.style.backgroundColor = greenColorHash;
-        });
+const changeColorCardButton = document.querySelector('#change-color-all-card');
+const productCards = document.querySelectorAll('.product-card__box');
+const yellowColorHash = '#FFFF00';
+const greenColorHash = '#00ff00';
+
+
+
+changeColorCardButton.addEventListener('click', () => {
+    productCards.forEach((card) => {
+        card.style.backgroundColor = greenColorHash;
     });
-}
+});
 
 const firstProductCard = document.querySelector('.product-card__box');
 const changeColorCardButtonTwo = document.querySelector('#change-color-first-card');
 
-if (changeColorCardButtonTwo) {
-    changeColorCardButtonTwo.addEventListener('click', () => {
-        if (firstProductCard) {
-            firstProductCard.style.backgroundColor = yellowColorHash;
-        }
-    });
-}
+
+changeColorCardButtonTwo.addEventListener('click', () => {
+    firstProductCard.style.backgroundColor = yellowColorHash;
+});
 
 const openGoogleButton = document.querySelector('#open-goole');
 
-if (openGoogleButton) {
-    openGoogleButton.addEventListener('click', () => {
-        const answer = confirm('Вы действительно хотите открыть Google?');
-        if (answer) {
-            window.location.href = 'https://google.com';
-        }
-    });
-}
 
-const outputLogButton = document.querySelector('#output-console-log');
+openGoogleButton.addEventListener('click', () => {
+    const answer = confirm('Вы действительно хотите открыть Google?');
+    if (answer) {
+        window.location.href = 'https://google.com';
+    }
+});
 
-outputLogButton.addEventListener('click', () => outputConsoleLog('дз №6'));
+const outputlogButton = document.querySelector('#output-console-log');
+
+outputlogButton.addEventListener('click', () => {
+    outputConsoleLog('Дз №6');
+});
 
 function outputConsoleLog(messager) {
-    alert(messager)
-    consolmessagere.log(messager)
+    alert(messager);
 }
