01d1306a5c9f73c833b101163ba071837aa978aa
jahar123 <ajbiaseno@gmail.com>
2026-08-09 16:26:21 +0300

    исправил ошибки

--- Model.js
+++ Model.js
@@ -1,45 +1,36 @@
 export default class Modal {
-    constructor(modalId) {
-       
-        this.modalElement = document.getElementById(modalId);
-        
-        if (this.modalElement) {
-           
-            this.initListeners();
-        } else {
-            console.error(`Модальное окно с id "${modalId}" не найдено.`);
-        }
+  constructor(modalId) {
+    this.modalElement = document.getElementById(modalId);
+    if (this.modalElement) {
+      this.initListeners();
+    } else {
+      console.error(`Модальное окно с id "${modalId}" не найдено.`);
     }
+  }
 
-   
-    open() {
-        if (this.modalElement) {
-            this.modalElement.classList.add('open'); 
-        }
+  open() {
+    if (this.modalElement) {
+      this.modalElement.classList.add('modal-showed');
     }
+  }
 
-   
-    close() {
-        if (this.modalElement) {
-            this.modalElement.classList.remove('open');
-        }
+  close() {
+    if (this.modalElement) {
+      this.modalElement.classList.remove('modal-showed');
     }
+  }
 
-    
-    isOpen() {
-        if (!this.modalElement) return false;
-        return this.modalElement.classList.contains('open');
-    }
+  isOpen() {
+    if (!this.modalElement) return false;
+    return this.modalElement.classList.contains('modal-showed');
+  }
 
-   
-    initListeners() {
-       
-        const closeButton = this.modalElement.querySelector('.close-btn');
-        
-        if (closeButton) {
-            closeButton.addEventListener('click', () => {
-                this.close(); 
-            });
-        }
+  initListeners() {
+    const closeButton = this.modalElement.querySelector('.modal__close');
+    if (closeButton) {
+      closeButton.addEventListener('click', () => {
+        this.close();
+      });
     }
-};
+  }
+}
